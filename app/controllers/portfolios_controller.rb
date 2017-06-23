365f1d346eb5d823d854daa692a2eee8cba3ea80
deepali <deepali.i.prasad@pwc.com>
2017-06-23 15:38:41 +0530

    Edit and update of portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -19,4 +20,23 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "The record successfully updated" }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
+
 end
